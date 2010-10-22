ae607fafdb9b9d705487a258334721814fb82c02
Dan Quellhorst <dan@abtain.com>
2010-10-22 02:28:05 -0500

    Added static assets to be synced to s3.

---
+++ static/scripts/gems.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# run with bash < <( curl http://q3.abtain.com/gems.sh )
+gem install rake
+gem install rdoc
+gem install bundler
+gem install wirble
+gem install awesome_print
+gem install hirb
+gem install what_methods
+gem install looksee
+gem install sketches
+gem install net-http-spy
+gem install map_by_method
+gem install rails
